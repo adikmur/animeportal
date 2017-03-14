15baacb2b05d841232316f876502b8e18b1d1587
Аида <muradkhanova.aida@gmail.com>
2017-03-14 21:59:17 +0300

    Добавление и дизайн главного окна и создание основных сущностей.

---
+++ AnimePortal/AnimePortal/Serial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimePortal
+{
+    class Serial
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Season> Seasons { get; set; }
+    }
+}
